60cca632fe64884e79c2b2d34e3baabc854b28b0
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-07-07 21:12:25 +0300

    adding comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let navar = document.querySelector (".navbar");
 window.addEventListener ('scroll', () => {
    header.classList.toggle('active', window-scrolly > 0);
 });
-
+//navigation bar
 menu.onclick = () => {
     navbar.classList.toggle('active');
 }
