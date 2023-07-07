f23ffc405988f3ac6643cabc1d405fdd2572a39e
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-07-07 19:06:20 +0300

    addition of menu link

---
+++ index.js
@@ -0,0 +1,10 @@
+let menu = document.queryselector("#menu-icon"); 
+let navar = document.querySelector (".navbar");
+
+window.addEventListener ('scroll', () => {
+   header.classList.toggle('active', window-scrolly > 0);
+});
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
